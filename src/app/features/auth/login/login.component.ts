432cfee4ad22f7c4597818fe8f08b6afb737cb4a
Prathamesh Khochade <65518339+bulbasor1509@users.noreply.github.com>
2026-08-15 16:05:22 +0530

    feat: added login page

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from '../../../shared/components/button/button.component';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ButtonComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required])
+  })
 
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
